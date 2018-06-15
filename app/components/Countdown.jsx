d3dd7dc0949f763888702f7d59cdb5449286c916
I Putu Arik Sasmita <ariksasmita@gmail.com>
2018-06-15 17:27:27 +0800

    Adding routes and links to Nav component

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,15 @@
+
+var React = require('react')
+
+var Countdown = React.createClass({
+  render() {
+    return (
+      <div>
+        Countdown Component
+      </div>
+    )
+  }
+})
+
+
+module.exports = Countdown
